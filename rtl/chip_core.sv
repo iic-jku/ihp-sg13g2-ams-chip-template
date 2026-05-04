70b35ed6fe4987dd2b0053c8f794bb8cd678d603
Simon Dorrer <simon.dorrer@jku.at>
2026-05-04 19:01:32 +0200

    added `rtl` folder

---
+++ rtl/chip_core.sv
@@ -0,0 +1,200 @@
+﻿// SPDX-FileCopyrightText: 2025-2026 The Chip Team
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+
+module chip_core #(
+    parameter NUM_INPUT_PADS,
+    parameter NUM_OUTPUT_PADS,
+    parameter NUM_BIDIR_PADS,
+    parameter NUM_ANALOG_PADS
+    )(
+	`ifdef USE_POWER_PINS
+    inout  wire VDD,
+    inout  wire VSS,
+    `endif
+	
+    input  logic clk,       // clock
+    input  logic rst_n,     // reset (active low)
+    
+    input  wire [NUM_INPUT_PADS-1 :0] input_in,   		// Input value
+    output wire [NUM_OUTPUT_PADS-1:0] output_out, 		// Output value
+    input  wire [NUM_BIDIR_PADS-1 :0] bidir_in,   		// Input value
+    output wire [NUM_BIDIR_PADS-1 :0] bidir_out,  		// Output value
+    output wire [NUM_BIDIR_PADS-1 :0] bidir_oe,   		// Output enable
+
+    inout  wire [NUM_ANALOG_PADS-1:0] analog_padres, 	// Analog (via pad secondary ESD protection)
+	inout  wire [NUM_ANALOG_PADS-1:0] analog_padbare    // Analog (direct pad connection with primary ESD protection)
+);
+	// ======================================================
+	// RISC-V CPU (Digital)
+	// ======================================================
+	logic si;
+	logic sclk;
+	logic sram_ce;
+	logic sda_o;
+	logic sda_oe;
+	logic scl;
+	logic tx;
+	logic [3:0] gpio_out;
+	logic analog_en;
+	logic I_ae;
+	logic ds_I_oe;
+	logic ds_I_p_o;
+    logic ds_I_n_o;
+	logic lo_I_oe;
+    logic lo_I_o;
+	logic lo_Ix_o;
+    logic lo_Qx_o;
+	logic lo_Q_o;
+	logic lo_Q_oe;
+	logic ds_Q_n_o;
+	logic ds_Q_p_o;
+	logic ds_Q_oe;
+	logic Q_ae;
+	
+	riscv_top riscv_top (
+		`ifdef USE_POWER_PINS
+        .VDD(VDD),
+		.VSS(VSS),
+		`endif
+		.clk(clk),
+		.reset(rst_n),
+		.so(input_in[5]),
+		.si(si),
+		.sclk(sclk),
+		.sram_ce(sram_ce),
+		.sda_i(bidir_in[8]),
+		.sda_o(sda_o),
+		.sda_oe(sda_oe),
+		.scl(scl),
+		.rx(input_in[4]),
+		.tx(tx),
+		.gpio_in(input_in[3:0]),
+		.gpio_out(gpio_out),
+		.analog_en(analog_en),
+		.I_ae(I_ae),
+		.ds_I_oe(ds_I_oe),
+		.ds_I_p_i(bidir_in[0]),
+		.ds_I_p_o(ds_I_p_o),
+		.ds_I_n_i(bidir_in[1]),
+		.ds_I_n_o(ds_I_n_o),
+		.lo_I_oe(lo_I_oe),
+		.lo_I_i(bidir_in[2]),
+		.lo_I_o(lo_I_o),
+		.lo_Ix_i(bidir_in[3]),
+		.lo_Ix_o(lo_Ix_o),
+		.lo_Qx_i(bidir_in[7]),
+		.lo_Qx_o(lo_Qx_o),
+		.lo_Q_i(bidir_in[6]),
+		.lo_Q_o(lo_Q_o),
+		.lo_Q_oe(lo_Q_oe),
+		.ds_Q_n_i(bidir_in[5]),
+		.ds_Q_n_o(ds_Q_n_o),
+		.ds_Q_p_i(bidir_in[4]),
+		.ds_Q_p_o(ds_Q_p_o),
+		.ds_Q_oe(ds_Q_oe),
+		.Q_ae(Q_ae)
+	);
+	// ======================================================
+	
+	// ======================================================
+	// IQ-Modulator (Analog)
+	// ======================================================
+	wire voutp_I_RF;
+	wire voutn_I_RF;
+	wire voutn_Q_RF;
+	wire voutp_Q_RF;
+	
+	iqmod_top iqmod_top (
+		`ifdef USE_POWER_PINS
+		.VDD(VDD),
+		.VSS(VSS),
+		`endif
+		.di_analog_en(analog_en),
+		.di_spdt_ctrl_I(I_ae),
+		.vinp_I(analog_padres[7]),
+		.di_ds_I_p(ds_I_p_o),
+		.vinn_I(analog_padres[6]),
+		.di_ds_I_n(ds_I_n_o),
+		.di_lo_I(lo_I_o),
+		.di_lo_Ix(lo_Ix_o),
+		.di_lo_Qx(lo_Qx_o),
+		.di_lo_Q(lo_Q_o),
+		.di_ds_Q_n(ds_Q_n_o),
+		.vinn_Q(analog_padres[1]),
+		.di_ds_Q_p(ds_Q_p_o),
+		.vinp_Q(analog_padres[0]),
+		.di_spdt_ctrl_Q(Q_ae),
+		.voutp_I_RF(voutp_I_RF),
+		.voutn_I_RF(voutn_I_RF),
+		.voutn_Q_RF(voutn_Q_RF),
+		.voutp_Q_RF(voutp_Q_RF)
+	);
+	// ======================================================
+	
+	// ======================================================
+	// Assignments
+    // ======================================================
+	// RISC-V CPU
+	// Outputs
+	assign output_out[8] = si;
+	assign output_out[7] = sclk;
+	assign output_out[6] = sram_ce;
+	assign output_out[5] = scl;
+	assign output_out[4] = tx;
+	assign output_out[3] = gpio_out[3];
+	assign output_out[2] = gpio_out[2];
+	assign output_out[1] = gpio_out[1];
+	assign output_out[0] = gpio_out[0];
+	
+	// Bidirectionals
+	// [8] sda
+	assign bidir_out[8] = sda_o;
+	assign bidir_oe[8]  = sda_oe;
+
+	// [7] lo_Qx
+	assign bidir_out[7] = lo_Qx_o;
+	assign bidir_oe[7]  = lo_Q_oe;
+
+	// [6] lo_Q
+	assign bidir_out[6] = lo_Q_o;
+	assign bidir_oe[6]  = lo_Q_oe;
+	
+	// [5] ds_Q_n
+	assign bidir_out[5] = ds_Q_n_o;
+	assign bidir_oe[5]  = ds_Q_oe;
+	
+	// [4] ds_Q_p
+	assign bidir_out[4] = ds_Q_p_o;
+	assign bidir_oe[4]  = ds_Q_oe;
+
+	// [3] lo_Ix
+	assign bidir_out[3] = lo_Ix_o;
+	assign bidir_oe[3]  = lo_I_oe;
+	
+	// [2] lo_I
+	assign bidir_out[2] = lo_I_o;
+	assign bidir_oe[2]  = lo_I_oe;
+	
+	// [1] ds_I_n
+	assign bidir_out[1] = ds_I_n_o;
+	assign bidir_oe[1]  = ds_I_oe;
+	
+	// [0] ds_I_p
+	assign bidir_out[0] = ds_I_p_o;
+	assign bidir_oe[0]  = ds_I_oe;
+
+	// IQ-Modulator
+	// Not connected analog pads (only padres or padbare is connected per analog pad)
+	// - analog_padres[2], analog_padres[3], analog_padres[4], analog_padres[5]
+	// - analog_padbare[0], analog_padbare[1], analog_padbare[6], analog_padbare[7]
+	
+	assign analog_padbare[5] = voutp_I_RF;
+	assign analog_padbare[4] = voutn_I_RF;
+	assign analog_padbare[3] = voutn_Q_RF;
+	assign analog_padbare[2] = voutp_Q_RF;
+	// ======================================================
+endmodule
+
+`default_nettype wire
---
+++ rtl/chip_top.sv
@@ -0,0 +1,295 @@
+﻿// SPDX-FileCopyrightText: 2025-2026 The Chip Team
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+
+module chip_top #(
+    // Power / ground pads for digital core / analog front-end
+    parameter NUM_VDD_PADS = 7, 		// 7 x VDD = 1.5V
+	parameter NUM_VSS_PADS = 9, 		// 9 x VSS
+
+    // Power/ground pads for I/O
+    parameter NUM_IOVDD_PADS = 3, 		// 3 x IOVDD = 3.3V
+    parameter NUM_IOVSS_PADS = 3, 		// 3 x IOVSS
+    
+    // Signal pads
+    parameter NUM_INPUT_PADS  = 6,		// 4 x GPIO inputs, so, rx
+    parameter NUM_OUTPUT_PADS = 9,		// 4 x GPIO outputs, si, sclk, sram_ce, scl, tx
+    parameter NUM_BIDIR_PADS  = 9,		// sda_i / sda_o, lo_I_i / lo_I_o, lo_Ix_i / lo_Ix_o, lo_Q_i / lo_Q_o, lo_Qx_i / lo_Qx_o, Q_p_i / Q_p_o, Q_n_i / Q_n_o, I_p_i / I_p_o, I_n_i / I_n_o
+    parameter NUM_ANALOG_PADS = 8 		// Vinp_I, Vinn_I, Voutp_I_RF, Voutn_I_RF, Voutn_Q_RF, Voutp_Q_RF, Vinn_Q, Vinp_Q
+    )(
+    `ifdef USE_POWER_PINS
+    inout wire IOVDD,
+    inout wire IOVSS,
+    inout wire VDD,
+    inout wire VSS,
+    `endif
+    inout  wire clk_PAD,
+    inout  wire rst_n_PAD,
+    inout  wire [NUM_INPUT_PADS-1 :0] input_PAD,
+    inout  wire [NUM_OUTPUT_PADS-1:0] output_PAD,
+    inout  wire [NUM_BIDIR_PADS-1 :0] bidir_PAD,
+    inout  wire [NUM_ANALOG_PADS-1:0] analog_PAD
+);
+	
+	// ======================================================
+    // Wires
+	// ======================================================
+	// Clock
+    wire clk_PAD2CORE;
+	
+	// Reset
+    wire rst_n_PAD2CORE;
+	
+	// Inputs
+    wire [NUM_INPUT_PADS-1 :0] input_PAD2CORE;
+
+    // Outputs
+    wire [NUM_OUTPUT_PADS-1:0] output_CORE2PAD;
+	
+	// Bidirectionals
+    wire [NUM_BIDIR_PADS-1 :0] bidir_PAD2CORE;
+    wire [NUM_BIDIR_PADS-1 :0] bidir_CORE2PAD;
+    wire [NUM_BIDIR_PADS-1 :0] bidir_CORE2PAD_OE;
+	
+	// Analog
+    wire [NUM_ANALOG_PADS-1:0] analog_PADRES;   // Vinp_I, Vinn_I, Vinn_Q, Vinp_Q
+    wire [NUM_ANALOG_PADS-1:0] analog_PADBARE;  // Voutp_I_RF, Voutn_I_RF, Voutn_Q_RF, Voutp_Q_RF
+	// ======================================================
+	
+	// ======================================================
+    // VDD - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_VDD_PADS; i++) begin : vdd_pads
+        (* keep *)
+        sg13g2_IOPadVdd vdd_pad  (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+            .vdd    (VDD),
+            .vss    (VSS)
+            `endif
+        );
+    end
+    endgenerate
+	// ======================================================
+	
+	// ======================================================
+    // VSS - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_VSS_PADS; i++) begin : vss_pads
+        (* keep *)
+        sg13g2_IOPadVss vss_pad  (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+			.vdd    (VDD),
+            .vss    (VSS)
+            `endif
+        );
+    end
+    endgenerate
+	// ======================================================
+	
+	// ======================================================
+    // IOVDD - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_IOVDD_PADS; i++) begin : iovdd_pads
+        (* keep *)
+        sg13g2_IOPadIOVdd iovdd_pad  (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+            .vdd    (VDD),
+            .vss    (VSS)
+            `endif
+        );
+    end
+    endgenerate
+	// ======================================================
+	
+	// ======================================================
+	// VSS IO - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_IOVSS_PADS; i++) begin : iovss_pads
+        (* keep *)
+        sg13g2_IOPadIOVss iovss_pad  (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+            .vdd    (VDD),
+            .vss    (VSS)
+            `endif
+        );
+    end
+	endgenerate
+	// ======================================================
+	
+    // ======================================================
+    // CLOCK - PAD INSTANCE
+	// ======================================================
+    sg13g2_IOPadIn clk_pad (
+        `ifdef USE_POWER_PINS
+        .iovdd  (IOVDD),
+        .iovss  (IOVSS),
+        .vdd    (VDD),
+        .vss    (VSS),
+        `endif
+        .p2c    (clk_PAD2CORE),
+        .pad    (clk_PAD)
+    );
+    // ======================================================
+	
+	// ======================================================
+    // RESET - PAD INSTANCE (active low)
+	// ======================================================
+    sg13g2_IOPadIn rst_n_pad (
+        `ifdef USE_POWER_PINS
+        .iovdd  (IOVDD),
+        .iovss  (IOVSS),
+        .vdd    (VDD),
+        .vss    (VSS),
+        `endif
+        .p2c    (rst_n_PAD2CORE),
+        .pad    (rst_n_PAD)
+    );
+	// ======================================================
+	
+	// ======================================================
+    // SIGNAL INPUTS - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_INPUT_PADS; i++) begin : inputs
+        sg13g2_IOPadIn input_pad (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+            .vdd    (VDD),
+            .vss    (VSS),
+            `endif
+            .p2c    (input_PAD2CORE[i]),
+            .pad    (input_PAD[i])
+        );
+    end
+    endgenerate
+	// ======================================================
+	
+	// ======================================================
+    // SIGNAL OUTPUTS - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_OUTPUT_PADS; i++) begin : outputs
+        sg13g2_IOPadOut30mA output_pad (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+            .vdd    (VDD),
+            .vss    (VSS),
+            `endif
+            .c2p    (output_CORE2PAD[i]),
+            .pad    (output_PAD[i])
+        );
+    end
+    endgenerate
+	// ======================================================
+	
+	// ======================================================
+    // SIGNAL BIDIRECTIONALS - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_BIDIR_PADS; i++) begin : bidirs
+        sg13g2_IOPadInOut30mA bidir_pad (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+            .vdd    (VDD),
+            .vss    (VSS),
+            `endif
+            .c2p    (bidir_CORE2PAD[i]),
+            .c2p_en (bidir_CORE2PAD_OE[i]),
+            .p2c    (bidir_PAD2CORE[i]),
+            .pad    (bidir_PAD[i])
+        );
+    end
+    endgenerate
+    // ======================================================
+	
+	// ======================================================
+    // SIGNAL ANALOG - PAD INSTANCES
+	// ======================================================
+    generate
+    for (genvar i=0; i<NUM_ANALOG_PADS; i++) begin : analogs
+        (* keep *)
+        sg13g2_IOPadAnalog analog_pad (
+            `ifdef USE_POWER_PINS
+            .iovdd  (IOVDD),
+            .iovss  (IOVSS),
+			.vdd    (VDD),
+            .vss    (VSS),
+            `endif
+            .pad    (analog_PAD[i]), // Analog (direct pad connection with primary ESD protection) --> SPECIAL net in OpenROAD and will be ignored during detailed routing. --> analog_PADBARE
+            .padres (analog_PADRES[i]),
+            .padbare(analog_PADBARE[i])
+        );
+    end
+    endgenerate
+	// ======================================================
+	
+    // ======================================================
+    // CORE DESIGN
+	// ======================================================
+	chip_core #(
+        .NUM_INPUT_PADS  (NUM_INPUT_PADS),
+        .NUM_OUTPUT_PADS (NUM_OUTPUT_PADS),
+        .NUM_BIDIR_PADS  (NUM_BIDIR_PADS),
+        .NUM_ANALOG_PADS (NUM_ANALOG_PADS)
+    ) i_chip_core (
+		`ifdef USE_POWER_PINS
+        .VDD   		    (VDD),
+        .VSS            (VSS),
+        `endif
+		
+        .clk            (clk_PAD2CORE),
+        .rst_n          (rst_n_PAD2CORE),
+		
+        .input_in       (input_PAD2CORE),
+		
+        .output_out     (output_CORE2PAD),
+        .bidir_in       (bidir_PAD2CORE),
+        .bidir_out      (bidir_CORE2PAD),
+        .bidir_oe       (bidir_CORE2PAD_OE),
+		
+        .analog_padres  (analog_PADRES),
+        .analog_padbare (analog_PADBARE)
+    );
+	// ======================================================
+	
+	// ======================================================
+    // LOGOS
+	// ======================================================
+    // JKU logo
+    (* keep *)
+    sg13g2_ip__jku jku_logo ();
+	// ======================================================
+
+    // JKU names
+    (* keep *)
+    sg13g2_ip__jku_names jku_names ();
+	// ======================================================
+	
+    // JMU CE logo
+	(* keep *)
+    sg13g2_ip__ce ce_logo ();
+	// ======================================================
+    
+    // JMU CE names
+	(* keep *)
+    sg13g2_ip__ce_names ce_names ();
+	// ======================================================
+endmodule
+
+`default_nettype wire
